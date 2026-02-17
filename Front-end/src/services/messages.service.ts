d49f2112928f413730ac775743f74059f145dbb2
eranga-de-saa <erangadesaa@gmail.com>
2026-02-17 11:57:57 -0800

    feat: implement Kubernetes deployment and service configurations for front-end and back-end, add Ingress resource, and update message handling in front-end service

--- Front-end/src/services/messages.service.ts
+++ Front-end/src/services/messages.service.ts
@@ -2,10 +2,10 @@
 import type { QueryMessage } from "../classes/QueryMessage";
 import type { Response } from "../classes/Response";
 
-const backendHost = import.meta.env.VITE_BACKEND_HOST || "localhost";
+// const backendHost = import.meta.env.VITE_BACKEND_HOST || "localhost";
 
 export async function sendMessage(content: QueryMessage): Promise<Response> {
-  const response = await fetch(`http://${backendHost}:3000/api/message`, {
+  const response = await fetch(`api/message`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(content),
